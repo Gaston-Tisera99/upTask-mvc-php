35e90e96aef83ba3b047964c36d42beb9e9dfd33
unknown <gastontisera94@gmail.com>
2024-01-19 19:19:41 -0300

    19/01

---
+++ controllers/TareaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+Class TareaController{
+
+    public static function index(){
+        
+    }
+
+    public static function crear(){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            $respuesta = [
+                'proyectoId' => $_POST['proyectoId']
+            ];
+
+            echo json_encode($respuesta);
+        }
+    }
+
+    public static function actualizar(){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            
+        }
+    }
+
+    public static function eliminar(){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            
+        }
+    }
+}
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -42,10 +42,82 @@
                     modal.remove();
                 }, 500);
             }
+
+            if(e.target.classList.contains('submit-nueva-tarea')){
+                submitFormularioNuevaTarea();
+            }
         })
 
-        document.querySelector('body').appendChild(modal);
+        document.querySelector('.dashboard').appendChild(modal);
     }
+
+        function submitFormularioNuevaTarea(){
+            const tarea = document.querySelector('#tarea').value.trim();
+
+            if(tarea === ''){
+                //mostrar una alerta de error
+
+                mostrarAlerta('El Nombre de la tarea es Obligatorio', 'error', 
+                document.querySelector('.formulario legend'));
+                return;
+            }
+
+            agregarTarea(tarea);
+        }
+
+        //muestra un mensaje en la interfaz
+        function mostrarAlerta(mensaje, tipo, referencia){
+            //previene la creacion de multiples alertas
+            const alertaPrevia = document.querySelector('.alerta');
+            if(alertaPrevia){
+                alertaPrevia.remove();
+            }
+
+
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('alerta', tipo);
+            alerta.textContent = mensaje;
+            // referencia.appendChild(alerta);
+            //inserta la alerta antes del legend
+            referencia.parentElement.insertBefore(alerta, referencia.nextElementSibling);
+
+            //eliminar la alerta despues de 5 segundos
+
+            setTimeout(()=>{
+                alerta.remove();
+            }, 5000)
+
+        }
+
+        //Consultar el servidor para añadir una nueva tarea al proyecto actual
+        async function agregarTarea(tarea){
+            //Construir la peticion
+            const datos = new FormData();
+            datos.append('nombre', tarea);
+            datos.append('proyectoId', obtenerProyecto());
+            
+
+            try {
+              const url = 'http://localhost:3000/api/tarea';
+              const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos
+              });
+              
+              const resultado = await respuesta.json();
+              console.log(resultado);
+              
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        function obtenerProyecto(){
+            const proyectoParams = new URLSearchParams(window.location.search)
+            const proyecto = Object.fromEntries(proyectoParams.entries());
+            return proyecto.id;
+        }
 })();
 
 
